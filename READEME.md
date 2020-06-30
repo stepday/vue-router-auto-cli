25b6ff982e6c4f89db92161227a2823aac169a56
stepday <stepday@users.noreply.github.com>
2020-07-01 00:49:40 +0800

    初始化

---
+++ READEME.md
@@ -0,0 +1,16 @@
+### 安装插件
+```
+npm i comander download-git-repo ora handlebars figlet clear chalk open watch -s
+```
+
+### 创建编译文件
+/bin/kkb.js
+
+### 配置package.json
+```
+"bin": {
+    "kkb": "./bin/kkb.js"
+  },
+```
+
+### 
---
+++ bin/kkb.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const program = require("commander");
+program.version(require("../package.json").version)
+
+program
+    .command("init <name>")
+    .description("init project   ")
+    .action(name => {
+        console.log("init " + name)
+    })
+
+program.parse(process.argv)
